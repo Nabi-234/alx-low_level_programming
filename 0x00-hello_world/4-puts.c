6af5da8f63340625b2d7bd5ac2555906c46fcbe5
Nabi-234 <kawtarbn74@gmail.com>
2023-08-10 15:23:35 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Size is not grandeur and territory does not make a nation
+ * Return: 0 (Good)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
