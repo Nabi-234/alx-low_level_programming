11d42758b8604a329428b547fa3327233fed550e
Nabi-234 <kawtarbn74@gmail.com>
2023-08-10 15:41:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Size is not grandeur and territory does not make a nation printf f function
+ * Return: 0 (Good)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
