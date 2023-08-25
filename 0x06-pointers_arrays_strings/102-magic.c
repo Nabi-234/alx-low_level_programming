fcf394b4ed5ebdfc7a3951dfbdcf2d9c4408013e
Nabi-234 <kawtarbn74@gmail.com>
2023-08-25 14:53:23 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ *  main - Entry point of the program
+ *
+ * This function demonstrates how to modify the value at a[2] using pointers.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -16,7 +22,7 @@ p = &n;
 * - only one statement
 * - you are not allowed to code anything else than this line of code
 */
-*(p + 5) = 98;
+*(p + 5) = 98;  /* This line updates the value at a[2] indirectly */
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
